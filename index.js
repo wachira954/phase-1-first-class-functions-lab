64032ebbdd447eb6e9efabb1cebd8d9130acfe79
wachira <wachiraa954@gmail.com>
2022-06-20 22:40:49 +0300

    commit tree 2f04da9ce2a8d975b77a1555bdf042fa3cfbdfb5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function receivesAFunction(spy){
+    console.log(spy());
+}
+receivesAFunction(function() {return spy})
+
+function returnANamedFunction(){
+    let fn;
+    if (fn = 'function'){
+        return function () {'return a function'};
+    }
+    else if (fn.name = '')
+    return 'return a named function'
+} 
+function returnsAnAnonymousFunction(){
+    let fn;
+    if (fn = 'function'){
+        return function (){'retuens a function'};
+    }
+    else if (fn.name ='')
+    return 'returns an anonymous function';
+}
